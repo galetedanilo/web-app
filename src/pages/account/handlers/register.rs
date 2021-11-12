82bfea57a79f552a80dbfcb48356f6488dd0dd73
galetedanilo <galetedanilo@gmail.com>
2021-11-11 22:13:43 -0300

    Create filter error message in backend to frontend response

--- src/pages/account/handlers/register.rs
+++ src/pages/account/handlers/register.rs
@@ -16,14 +16,30 @@ pub async fn register_new_account_form(template: web::Data<Tera>) -> Result<Http
     Ok(HttpResponse::Ok().body(render))
 }
 
-pub async fn register_new_account(form: web::Form<NewAccountForm>) -> Result<HttpResponse, Error> {
+pub async fn register_new_account(form: web::Form<NewAccountForm>, template: web::Data<Tera>) -> Result<HttpResponse, Error> {
 
     match form.validate() {
         Ok(_) => Ok(
             HttpResponse::Ok().body("Cadastrar")
         ),
         Err(err) => {
-            Ok(HttpResponse::Ok().body(format!("Error: {}", err)))
+            
+            let mut err_resp: Vec<String> = Vec::new();
+            let mut context = Context::new();
+
+            context.insert("page_title", "Create New Account");
+
+            for (_key, value) in &err.field_errors() {
+                for ex in value.into_iter() {
+                    err_resp.push(ex.to_string());
+                }
+            }
+
+            context.insert("message_error", &err_resp);
+
+            let render = template.render("account/register.html", &context).map_err(error::ErrorInternalServerError)?;
+
+            Ok(HttpResponse::Ok().body(render))
         },
     }
 
