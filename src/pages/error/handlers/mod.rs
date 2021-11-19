2dcb57020e97379e8444ce15fbef440471fd541e
galetedanilo <galetedanilo@gmail.com>
2021-11-18 21:20:09 -0300

    Fixing ErrorHandler
    In this commit, fixing response to the error handler, retur bad request for status code 400, not found for status code 404, method not allowed for status code 405 and internal server error for default

--- src/pages/error/handlers/mod.rs
+++ src/pages/error/handlers/mod.rs
@@ -6,7 +6,7 @@ use tera::{Context, Tera};
 pub fn bad_request<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
 
     render_page_error(
-        res, 
+        res, 400,
         "Ooops 400 - Bad Request", 
         "Bad Request!",
         "The server could not understand the request."
@@ -16,7 +16,7 @@ pub fn bad_request<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerRespon
 pub fn page_not_found<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
 	
     render_page_error(
-        res, 
+        res, 404,
         "Ooops 404 - Page Not Found", 
         "Ooops...",
         "The page you were looking for doesn't exist. You may have mistyped the address or the page may have moved."
@@ -26,7 +26,7 @@ pub fn page_not_found<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerRes
 pub fn method_not_allowed<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
 
     render_page_error(
-        res, 
+        res, 405,
         "Ooops 405 - Method Not Allowed", 
         "Not Allowed!",
         "The page you are looking for cannot be displayed because an invalid method is being used"
@@ -36,14 +36,14 @@ pub fn method_not_allowed<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandle
 pub fn internal_server_error<B>(res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>> {
     
     render_page_error(
-        res, 
+        res, 500,
         "Ooops 500 - Internal Server Error", 
         "How embarrassing!",
         "Looks like something weird happened while processing your request. Please try again in a few moments."
     )
 }
 
-fn render_page_error<B>(res: dev::ServiceResponse<B>, title: &str, error: &str, message: &str) -> Result<ErrorHandlerResponse<B>> {
+fn render_page_error<B>(res: dev::ServiceResponse<B>, code: u16, title: &str, error: &str, message: &str) -> Result<ErrorHandlerResponse<B>> {
 
     let request = res.request();
 
@@ -64,14 +64,19 @@ fn render_page_error<B>(res: dev::ServiceResponse<B>, title: &str, error: &str, 
             context.insert("message", message);
     
             let render = tera.render("error/error.html", &context);
-    
+
             match render {
                 Ok(render) => Ok(
                     ErrorHandlerResponse::Response(
                         res.into_response(
-                            HttpResponse::InternalServerError()
-                                .body(render)
-                                .into_body(),
+
+                            match code {
+                                400 => HttpResponse::BadRequest().body(render).into_body(),
+                                404 => HttpResponse::NotFound().body(render).into_body(),
+                                405 => HttpResponse::MethodNotAllowed().body(render).into_body(),
+                                _ => HttpResponse::InternalServerError().body(render).into_body(),
+                            }
+ 
                         )
                     )
                 ),
